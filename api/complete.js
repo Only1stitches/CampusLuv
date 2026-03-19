a9b49436c0f47763bce1bcf9fb4f211cb4f4c012
Only1stitches <uyistitches@gmail.com>
2026-03-19 14:54:17 +0100

    Update complete.js

--- api/complete.js
+++ api/complete.js
@@ -1,4 +1,6 @@
 export default async function handler(req, res) {
+  if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
+
   const { paymentId, txid } = req.body;
   const apiKey = "az4vrfje7tpvc2kmehv2hzrawklj7hlqruirzyarrlpmuhq6jzcspenwu8zoqlqq";
 
@@ -11,10 +13,9 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify({ txid })
     });
-    
     const data = await response.json();
-    res.status(200).json(data);
+    return res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    return res.status(500).json({ error: error.message });
   }
 }
