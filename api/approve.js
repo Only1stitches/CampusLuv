e926a677247abc5a9b6c018e77204dd0f826d863
Only1stitches <uyistitches@gmail.com>
2026-03-19 15:09:09 +0100

    Update approve.js

--- api/approve.js
+++ api/approve.js
@@ -1,16 +1,15 @@
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
-
-  const { paymentId, txid } = req.body;
+  
+  const { paymentId } = req.body;
 
   try {
-    const response = await fetch(`https://api.minepi.com/v2/payments/${paymentId}/complete`, {
+    const response = await fetch(`https://api.minepi.com/v2/payments/${paymentId}/approve`, {
       method: 'POST',
       headers: { 
         'Authorization': 'Key az4vrfje7tpvc2kmehv2hzrawklj7hlqruirzyarrlpmuhq6jzcspenwu8zoqlqq',
         'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ txid })
+      }
     });
     const data = await response.json();
     return res.status(200).json(data);
